b08ff522b66bf5e4d4bd5397cba486e1bd1982a1
priyanka <priya.nil1811@gmail.com>
2020-12-02 20:31:33 -0500

    resolve the issue with more than two selection

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -13,6 +13,7 @@ window.addEventListener('DOMContentLoaded', function() {
       // If we haven't selected 2 cards yet, add the current card to the
       // collection of selected cards and apply the correct CSS class.
       if (selectedCards.length < 2) {
+        selectedCards.push(card1);
         card.classList.add('is-selected');
       }
 
@@ -32,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function() {
         // Regardless of whether or not the cards match, deselect them and reset
         // the collection of matched cards.
         card1.classList.remove('is-selected');
-        card3.classList.remove('is-selected');
+        card2.classList.remove('is-selected');
         selectedCards = [];
       }
 
